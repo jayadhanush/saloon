11d1819a2b98074e4f2230168c59d6951ce5db06
jayadhanush <jayadhanushravichandran@gmail.com>
2025-02-13 11:01:24 +0530

    regular commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 
-import './App.css'
-
+import './App.css';
+import HomePage from "./pages/HomePage"
+function Navbar() {
+  return (
+    <nav className="bg-gray-900 text-white p-4 shadow-md">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="text-xl font-semibold">Salon Booking</h1>
+        <ul className="flex space-x-6">
+          <li><Link to="/" className="hover:text-gray-300">Home</Link></li>
+          <li><Link to="/HomePage" className="hover:text-gray-300">Book Appointment</Link></li>
+          
+        </ul>
+      </div>
+    </nav>
+  );
+}
 function App() {
 
   return (
     <>
-      <h1>Welcome Jayadhanush</h1>
+      
     </>
   )
 }
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+
+function HomePage(){
+    return(
+        <>
+        <h1>Home Page</h1>
+        <p>This is the homepage</p>
+        </>
+    )
+}
+export default HomePage;
